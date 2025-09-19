9fb2295c45644a8d534caf08d4a13d44974ea6f8
Guilherme Kobylansky <06664631075@senacrs.edu.br>
2025-09-19 09:53:32 -0300

    Refinamento do refinamento
    Melhoria do refinamento

--- CRUD/lista-producao.php
+++ CRUD/lista-producao.php
@@ -39,7 +39,7 @@ include_once './include/header.php';
             <td>'.$row['ProducaoID'].'</td>
             <td>'.$row['produto_nome'].'</td>
             <td>'.$row['funcionario_nome'].'</td>
-            <td>'.str_replace('-', '/', $row['DataProducao']).'</td>
+            <td>'.str_replace('-', '/', date("d/m/Y", strtotime($row['DataProducao']))).'</td>
             <td>
             <a href="salvar-producao.php?id='.$row['ProducaoID'].'" class="btn btn-edit">Editar</a>
             <a href="./action/producao.php?id='.$row['ProducaoID'].'&acao=excluir" class="btn btn-delete" onclick="return confirm(\'Tem certeza que deseja excluir?\')">Excluir</a>
